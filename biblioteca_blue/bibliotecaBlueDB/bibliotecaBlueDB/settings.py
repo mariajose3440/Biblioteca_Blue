cd8e44e6599487d93707e6ec958decf10c902266
mariajose3440 <mjrodriguez@unl.edu.ec>
2026-06-22 18:40:56 -0500

    Conexion con mysql, crear superusuario

--- biblioteca_blue/bibliotecaBlueDB/bibliotecaBlueDB/settings.py
+++ biblioteca_blue/bibliotecaBlueDB/bibliotecaBlueDB/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Lector',
+    'Prestamo',
+    'modelo',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +77,12 @@ WSGI_APPLICATION = 'bibliotecaBlueDB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'biblioteca_blue',
+        'USER': 'bluebul_bibliotecaBlue',
+        'PASSWORD': 'Bluebul12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
